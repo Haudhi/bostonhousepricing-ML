34c5f23851bfc54d31dd75467dc8e355065e4940
@haudhiizza <haudhi.izzah@salt.co.id>
2023-11-30 15:53:33 +0700

    add app py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import pandas as pd
 
 app=Flask(__name__)
 # load the model
-regmodel=pickle.load(open('regmodel.pkl'))
+regmodel=pickle.load(open('regmodel.pkl', 'rb'))
+scaler=pickle.load(open('scaling.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,3 +17,11 @@ def home():
 def predict_api():
     data=request.json['data']
     print(data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1, -1))
+    output=regmodel.predict(new_data)
+    print(output)
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
